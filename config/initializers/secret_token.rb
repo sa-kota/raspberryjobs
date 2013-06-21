3fa694e8aee34bec755094b4e5612230af6b6a16
Sandesh Kota <sandeshjkota@gmail.com>
2013-06-21 12:27:48 +0530

    For raspberry!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raspberry::Application.config.secret_token = '0141f7e468d4daca5e6505351df697c945b49b9b142cd6bbe5ef3510d31f7a5fcbd10214e08911747363bf4acc9058543bd480c046b692f984bba4a93028b386'
