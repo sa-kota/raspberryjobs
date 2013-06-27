916a98eb0063d8c1daff625dd220a3d2cf4bd9ea
Sandesh Kota <sandeshjkota@gmail.com>
2013-06-27 19:07:25 +0530

    Search Box

--- app/controllers/jobresults_controller.rb
+++ app/controllers/jobresults_controller.rb
@@ -4,8 +4,24 @@ class JobresultsController < ApplicationController
   include HTTParty
   base_uri "http://api.careerbuilder.com"
 
-  def index
-    @jobresults = jobsearch()
+    def index
+      if !params[:kw].blank? && !params[:loc].blank?
+
+        @jobresults = jobsearch(:keywords => params[:kw],:Location => params[:loc])
+
+      elsif  !params[:kw].blank? && params[:loc].blank?
+
+        @jobresults = jobsearch(:keywords => params[:kw] )
+
+      elsif  params[:kw].blank? && !params[:loc].blank?
+
+        @jobresults = jobsearch(:Location => params[:loc])
+
+      else
+
+        @jobresults = jobsearch(:keywords => "")
+
+      end
   end
 
 end
