9991887305ea02cd0457564d97040141f48efddc
Sandesh Kota <sandeshjkota@gmail.com>
2013-06-28 15:33:50 +0530

    Few more changes

--- app/controllers/jobdetails_controller.rb
+++ app/controllers/jobdetails_controller.rb
@@ -13,8 +13,6 @@ class JobdetailsController < ApplicationController
 
   def apply
     @jobDID = params[:jobDID]
-
-    #@applyformurl = 'http://api.careerbuilder.com/v1/application/form?DeveloperKey=WDHL2LS5XZF8GYFHK1SB&JobDID=J3H1CQ6G7W3C751X29V'
     @applyformurl = 'http://api.careerbuilder.com/v1/application/form?DeveloperKey=WDHL2LS5XZF8GYFHK1SB&JobDID='.to_s() + @jobDID
   end
 
